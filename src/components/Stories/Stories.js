1f6fc74bef50eb39d07f44f94c6cc38871e71868
Ryan Larkin <rlarkin212@gmail.com>
2019-12-07 16:10:21 +0000

    please :(

--- src/components/Stories/Stories.js
+++ src/components/Stories/Stories.js
@@ -1,4 +1,4 @@
-import React, { Component } from './node_modules/react'
+import React, { Component } from 'react'
 import StoriesTable from '../storiesTable/StoriesTable'
 
 class Stories extends Component {
